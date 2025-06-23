010e0d9a140d2071e153a0bd22ed9c4ff18b59ff
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-06-23 23:23:51 +0530

    filtering name

---
+++ filtered_name/index.js
@@ -0,0 +1,22 @@
+const person = ["Priya","Khushi","Sakshi","Manish","Yash","Archana","Ayushi","Kajal"];
+const box = document.getElementById("search");
+const nameList = document.getElementById("namelist");
+
+function display(filtered){
+    nameList.innerHTML="";
+    filtered.forEach(function(name){
+         const li = document.createElement("li");
+    li.textContent = name;
+    nameList.appendChild(li);
+        });
+}
+
+display(person);
+box.addEventListener("input", function () {
+  const keyword = box.value.toLowerCase(); 
+  const filtered = person.filter(function(name) {
+    return name.toLowerCase().includes(keyword);
+  });
+
+  display(filtered); 
+});
