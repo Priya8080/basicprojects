06b1bf6bb6738da50986828ef82005ec9d439cb9
Priya Kumari <kumaripriya8084785476@gmail.com>
2025-06-25 13:56:41 +0530

    password strenth

--- password_strength/index.js
+++ password_strength/index.js
@@ -4,16 +4,20 @@ document.getElementById("inpassword").addEventListener("input",function(){
     let charC = password.length;
     if(charC<=6){
         document.getElementById("result").innerText = "Weak Password";
+        document.getElementById("inpassword").style.backgroundColor = "red";
     }
     
     else if(/[a-zA-Z]/.test(password) && /\d/.test(password) && /[^a-zA-Z0-9]/.test(password) && charC>=8){
         document.getElementById("result").innerText = "Strong Password";
+        document.getElementById("inpassword").style.backgroundColor = "green";
     }
   else if(/[a-zA-Z]/.test(password) && /\d/.test(password) && charC>=6){
         document.getElementById("result").innerText = "Medium Password";
+        document.getElementById("inpassword").style.backgroundColor = "orange";
     }
     else{
         document.getElementById("result").innerText = "Weak Password";
+        document.getElementById("inpassword").style.backgroundColor = "red";
     }
 });
 };
